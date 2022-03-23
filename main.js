755b68167d0f4d0ef16452f44e0323444ba4b6f9
Chewbacaguy <santiagot2000@gmail.com>
2022-03-23 16:44:11 -0600

    2 files modified

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-import './style.css'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+async function fetchDataFromAPIEndPoint() {
+  const cards = await fetch('api/fetchNotion').then((res) =>
+    res.json().then((data) => data.results)
+  );
+  document.querySelector('.card-container').innerHTML = cards
+    .map(
+      (card) => `
+    <article class="card">
+    <img
+      src="${card.properties.Image.files[0].external.url}"
+      alt="${card.properties.Name.title[0].plain_text}"
+      class="card__image"
+    />
+    <h2 class="card__heading">${card.properties.Name.title[0].plain_text}</h2>
+    <div class="card__price">
+      <p>${card.properties.Price.rich_text[0].plain_text}</p>
+      <a href="${card.properties.Link.url}" class="card__btn">${card.properties.Btb_text.rich_text[0].plain_text}</a>
+    </div>
+  </article>
+  `
+    )
+    .join('');
+}
+
+fetchDataFromAPIEndPoint();
